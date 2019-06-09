e99b87889b7c10b6d0b90ae7144a0a37efafe2f5
BiaobiaoZhangZiyang <996478975@qq.com>
2019-06-09 16:51:21 +0800

    first change

---
+++ resource/day01/day-01.py
@@ -0,0 +1,15 @@
+"""
+Title: Learn turtle
+Version: 0.1
+Author: zzy
+"""
+import turtle
+turtle.pensize(4)
+turtle.pencolor('blue')
+turtle.right(60)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.mainloop()
